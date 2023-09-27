630430be4d9a2b296415bdd3cf2da49d5339b224
Bassam Husain <bassam.husain@gmail.com>
2023-09-26 23:26:04 -0400

    Adjust .eslintrc.json.

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+// import { Client } from 'pg';
+
+// const Users = pgTable('User', {
+//   id: serial('id').primaryKey(),
+//   name: text('name'),
+//   email: text('email'),
+//   // todoList
+// });
