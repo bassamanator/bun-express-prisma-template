a42192b9e028c0827bb53b0698f5c00aa6b96025
Bassam Husain <bassam.husain@gmail.com>
2023-09-27 02:43:52 -0400

    Adjust tsconfig.json.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+// import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const User = pgTable('User', {
-  id: serial('id').primaryKey(),
-  name: text('name'),
-  email: text('email'),
-  // todoList
-});
+// export const User = pgTable('User', {
+//   id: serial('id').primaryKey(),
+//   name: text('name'),
+//   email: text('email'),
+//   // todoList
+// });
